bc200d5813976fa1a22c179248108cd03d723460
viktor-shmigol <viktor@active-bridge.com>
2017-06-27 23:01:09 +0300

    Update readme

--- tools/copy-package.js
+++ tools/copy-package.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 let resizable = fs.readFileSync('package.json').toString();
+let readme = fs.readFileSync('README.MD').toString();
 fs.writeFileSync('dist/package.json', resizable);
+fs.writeFileSync('dist/README.MD', readme);
